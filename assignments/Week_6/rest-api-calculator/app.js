1339b57ea48ca6754b3b8c84a382ba83656c9dfd
shivamcs <shivamcs0049@gmail.com>
2019-03-29 17:13:06 +0530

    sub,mul,div routes added

--- assignments/Week_6/rest-api-calculator/app.js
+++ assignments/Week_6/rest-api-calculator/app.js
@@ -12,7 +12,7 @@ app.post("/add",function(request,response){
     var num1 = request.body.num1
     var num2 = request.body.num2
     console.log("Value of num1 :"+num1+" num2 :"+num2)
-    var result = num1 + num2
+    var result = parseInt(num1) + parseInt(num2)
     console.log("Result : "+result)
     var data = {
         num1:num1,
@@ -22,5 +22,49 @@ app.post("/add",function(request,response){
     console.log(data)
     response.json(data)
 
+})
+
+app.post("/sub",function(request,response){
+    var num1 = request.body.num1
+    var num2 = request.body.num2
+    
+    var result = parseInt(num1) - parseInt(num2)
+    
+    var data = {
+        num1:num1,
+        num2:num2,
+        result:result
+    }
+    
+    response.json(data)
+    
+});
+app.post("/mul",function(request,response){
+    var num1 = request.body.num1
+    var num2 = request.body.num2
+    
+    var result = parseInt(num1) * parseInt(num2)
+    
+    var data = {
+        num1:num1,
+        num2:num2,
+        result:result
+    }
+    
+    response.json(data)
+})
+app.post("/div",function(request,response){
+    var num1 = request.body.num1
+    var num2 = request.body.num2
+    
+    var result = parseInt(num1) / parseInt(num2)
+    
+    var data = {
+        num1:num1,
+        num2:num2,
+        result:result
+    }
+    
+    response.json(data)
 })
 app.listen(3000)
