ccf91d63f712649444395a5c1c0866b0080d17ca
shivamcs <shivamcs0049@gmail.com>
2019-04-03 17:09:11 +0530

    callback function added

--- assignments/coding-challenge/coding-challenge-14/public/js/script.js
+++ assignments/coding-challenge/coding-challenge-14/public/js/script.js
@@ -24,7 +24,7 @@ const giveValue = () => {
 /* 
     validate() takes argument username and password and validate whether it is null or not 
 */
-const validate = (userName, password) => {
+const validate = (userName, password, fun) => {
 
     var message = ''
 
@@ -32,75 +32,75 @@ const validate = (userName, password) => {
         return false;
     }
     var data = {
-        name:userName,
-        password:password
+        name: userName,
+        password: password
     }
 
     var request = new XMLHttpRequest();
-    request.open("post","/login");
-    request.setRequestHeader("content-type","application/json");
+    request.open("post", "/login");
+    request.setRequestHeader("content-type", "application/json");
     request.send(JSON.stringify(data))
-    request.onreadystatechange = function(){
-        if(request.readyState == 4 && request.status == 200){
-             message  = request.responseText
-             console.log("Response From server :" +message)
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            message = request.responseText
+            console.log("Response From server :" + message)
+            fun(message)
         }
     }
-    if(message == "Success"){
-        console.log("returned true")
-        return true;
-    }else if(message == "Failed"){
-        console.log("returned false")
-        return false;
-    }
-    
+
+
 }
 /* 
   login() checks whether username and password is valid or not 
   If username = "shivam" and password ="password" 
   then it display login successfull message 
   of display login failed message
+  
 */
+
+var validateCallback = function (message) {
+
+    if (message == "Success") {
+        displayMessage("success")
+
+    } else if (message == "Failed") {
+        displayMessage("error")
+
+    }
+
+}
+
 const login = () => {
     event.preventDefault();
 
     let userName = giveValue().userName;
     let password = giveValue().password;
 
-    if (validate(userName, password)) {
-
-        console.log("login successfull")
-        displayMessage("success")
-            
-    }
-    else {
-        console.log("Login  failed")
-        displayMessage("error")
-    }
+    validate(userName, password,validateCallback)
 
 }
 /*
   displayMessage() takes message as argument and  style it and display it on screen
 */
-const displayMessage = (message)=>{
-    if(message === "success" || message === ""){
-       
-        let error =  document.getElementById("error");
+const displayMessage = (message) => {
+    if (message === "success" || message === "") {
+
+        let error = document.getElementById("error");
         if (window.getComputedStyle(error).display === "block") {
             error.style.display = "none";
         }
         document.getElementById("success").style.display = "block"
     }
-    if(message === "error" || message === ""){
-      
-        let success =  document.getElementById("success");
+    if (message === "error" || message === "") {
+
+        let success = document.getElementById("success");
         if (window.getComputedStyle(success).display === "block") {
             success.style.display = "none";
         }
         document.getElementById("error").style.display = "block"
     }
-    
-    
+
+
 }
 /*
 Handle login button click it executed login function 
