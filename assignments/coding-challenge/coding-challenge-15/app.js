6330df8bdd30e344ceffd9ab1ad42f853f0bd269
shivamcs <shivamcs0049@gmail.com>
2019-04-04 23:35:19 +0530

    programm corrected

--- assignments/coding-challenge/coding-challenge-15/app.js
+++ assignments/coding-challenge/coding-challenge-15/app.js
@@ -3,41 +3,38 @@ var fs = require("fs");
 var mongodb = require("mongodb")
 
 var DB;
-var Books = []
-
-var mongoClient = new mongodb.MongoClient("mongodb://localhost:27017/library", { useNewUrlParser: true });
-mongoClient.connect(function (error) {
-    if (error) {
-        console.log("Error connecting to the database.");
-    } else {
-        console.log("DB connection established.");
-        DB = mongoClient.db("library");
-    }
-});
-
+var Books;
 
 var readFile = function (fileName, callBackFun) {
+    console.log("File Reading started ....")
     fs.readFile(fileName, function (err, data) {
         if (err) {
             console.log("Could not read file");
         }
-        // console.log(data.toString())
-        Books.push(data)
+        Books = JSON.parse(data)
+
         callBackFun(Books);
-    });   
+    });
 }
 
-var putDataIntoDb = function (dataArray) {
-    console.log("putDataIntoDb called...")
-    var book;
-    console.log("books array size is :" + dataArray.length)
-    for (var i = 0; i < dataArray.length; i++) {
-        console.log(dataArray[i].author+" "+dataArray[i].country)
-        book = {
-            book: dataArray[i]
+var putDataIntoDb = function (books) {
+    console.log("Books length :"+books.length)
+    var mongoClient = new mongodb.MongoClient("mongodb://localhost:27017/library", { useNewUrlParser: true });
+    mongoClient.connect(function (error) {
+        if (error) {
+            console.log("Error connecting to the database.");
+        } else {
+            console.log("DB connection established.");
+            DB = mongoClient.db("library");
+            DB.collection("books").insertMany(books, function (error, result) {
+                if (error) {
+                    console.log("Failed to Store Books in DataBase.")
+                    return;
+                }
+                console.log("Books inserted ..Books Size :" + books.length)
+            });
         }
-        console.log(book.toString())
-    }
+    });
 }
 
 readFile("books.json", putDataIntoDb);
