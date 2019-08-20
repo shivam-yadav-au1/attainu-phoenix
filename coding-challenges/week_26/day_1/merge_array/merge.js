f149ca8f176f62d630751c7c98016f1c978159b6
shivam <shivamcs0049@gmail.com>
2019-08-20 21:09:01 +0530

    merge of array did with merge sort concept updated

--- coding-challenges/week_26/day_1/merge_array/merge.js
+++ coding-challenges/week_26/day_1/merge_array/merge.js
@@ -1,36 +1,33 @@
+function mergeArray(a, b) {
 
+    let merged = [];
+    while (a.length && b.length) {
+        let smalllest;
+        if (a[0] < b[0]) {
+            smallest = a.shift();
+        } else {
+            smallest = b.shift();
+        }
+        merged.push(smallest)
 
-function mergeArray(a,b){
-
-	
-	let outPut = [];
-	for(var i=0;i<a.length;i++){
-		outPut.push(a[i]);
-	}
-	for(var j=0;j<b.length;j++){
-		outPut.push(b[j]);
-	}
-	for(var i=0;i<outPut.length;i++){
-		for(var j=0;j<outPut.length;j++){
-
-			if(outPut[j] > outPut[j+1]){
-			var temp = outPut[j];
-			outPut[j] = outPut[j+1];
-			outPut[j+1] = temp;
-			}
-		}
-		
-	}
-return outPut;
+    }
+    if (a.length) {
+        merged = merged.concat(a);
+    }
+    if (b.length) {
+        merged = merged.concat(b);
+    }
+    return merged;
 
 
 }
 
-function main(){
-	let a = [4,8,56,23,9,66];
-	let b = [6,2,0,1,13,4];
-	let outPut = mergeArray(a,b);
-	console.log(outPut);
+function main() {
+    let a = [4, 8, 56, 23, 9, 66];
+    let b = [6, 2, 0, 1, 13, 4];
+    console.log(a,b)
+    let outPut = mergeArray(a, b);
+    console.log(outPut);
 }
 
 main();
